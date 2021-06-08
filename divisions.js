6d9e8db1f04d3c9d4c3a14b90035343a413ded18
Atulya1110 <76092426+Atulya1110@users.noreply.github.com>
2021-06-08 17:16:36 +0800

    Add files via upload

--- divisions.js
+++ divisions.js
@@ -1,19 +1,18 @@
-class Divisions{
-
-    constructor(x,y,w,h){
+class Divisions {
+    constructor(x, y, w, h) {
         var options = {
+
             isStatic: true
         }
-        this.body = Bodies.rectangle(x,y,w,h,options);
+        this.body = Bodies.rectangle(x, y, w, h, options);
         this.w = w;
         this.h = h;
-        World.add(world,this.body)
+        World.add(world, this.body);
     }
-    display(){
+    display() {
         var pos = this.body.position;
         rectMode(CENTER);
         fill("white");
-        stroke("red");
-        rect(pos.x,pos.y,this.w,this.h);
+        rect(pos.x, pos.y, this.w, this.h);
     }
-}
+};
